b9b7279224a018dfe58a0fcfc4029178b047ada7
Mabel Samuel <mapleberrysamuel@gmail.com>
2025-11-25 21:46:58 +0100

    feat: added a little padding at the bottom of the footer

--- src/app/_components/FooterSection.tsx
+++ src/app/_components/FooterSection.tsx
@@ -5,7 +5,7 @@ export default function FooterSection() {
   const currentYear = new Date().getFullYear();
   const { theme } = useThemeStore();
   return (
-    <div className='flex justify-center items-center'>
+    <div className='flex justify-center items-center pb-8'>
       <p className={`${theme == 'dark' ? 'text-white' : ''}`}>Copyright © {currentYear} Mabel Samuel - All Rights Reserved.</p>
     </div>
   )
